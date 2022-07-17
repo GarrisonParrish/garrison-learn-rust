41c5dd8f8308b059b7051d118cc6a24faf755e99
Garrison Parrish <tpgarris@live.unc.edu>
2022-07-17 17:22:56 -0400

    notes

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,9 @@
-use std::io;
 use rand::Rng;  // random number generator
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
+    /*
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -15,7 +17,16 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("failed to read line");
+    */
 
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+
+        
+    }
     
 }
